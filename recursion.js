7581d9b1888cdd6958020a323d0da44cf3afffc8
advikgupta <advikgupta@gmail.com>
2025-05-16 02:49:13 +0530

    initial commit

---
+++ recursion.js
@@ -0,0 +1,25 @@
+function countDown(num) {
+  if (num <= 0) {
+    console.log("Done!");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(5);
+
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+
+console.log(sumRange(4));
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5));
